f4b2125fb9aa9828e79791aadfdabc8864fb6a4b
Erica Liu <ericaenjoy3@gmail.com>
2018-01-30 21:35:29 -0500

    call

--- callGRFLoop.R
+++ callGRFLoop.R
@@ -1,11 +1,11 @@
 #!/usr/bin/env Rscript
 
-libfs <- dir(path.expand("~/ComScripts/RPack/GRFLoop/R"), "*.R", recursive = TRUE, full.names = TRUE)
+libfs <- dir(path.expand("~/athena/ComScripts/RPack/GRFLoop/R"), "*.R", recursive = TRUE, full.names = TRUE)
 idx <- grepl("class", libfs, ignore.case = TRUE)
 libfs <- libfs[c(which(idx), which(!idx))]
-for (i in seq_along(libfs)) {
-  message("source file ", i, " ",libfs[i])
-  source(f)
+for (j in seq_along(libfs)) {
+  message("source file ", j, " ",libfs[j])
+  source(libfs[j])
 }
 # OP either connect or heatmap
 OP <- "heatmap"
