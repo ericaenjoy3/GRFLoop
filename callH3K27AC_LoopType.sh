c2b990bf1be847b3ab9e50c62d2ea3159d95c21a
Erica Liu <ericaenjoy3@yahoo.co.uk>
2018-02-13 17:24:42 -0500

    update bash script

--- callH3K27AC_LoopType.sh
+++ callH3K27AC_LoopType.sh
@@ -4,13 +4,13 @@ SDR=~/athena/ComScripts/RPack/GRFLoop
 DIN=~/athena/Andreas_H3K27AC_HICHIP/doc
 chipf=()
 
-for type in ESC; do 
+for type in MEF; do 
 	chipf+=(~/athena/CHIP/CHIP_seq/MergeRep/mergepeak/ReProgram/H3K27AC_${type}/H3K27AC_${type}_common.bed)
 done
 
-sm=ESC
+sm=MEF
 cmd="Rscript --no-save --no-restore $SDR/H3K27AC_LoopType.R \
 --fin $DIN/${sm}_H3K27AC.txt \
 --chipf ${chipf[@]} \
 --bedout $DIN/${sm}_H3K27AC_10KB_LoopType.txt"
-echo $cmd
+$cmd
