76d3a2979da73c468c04bb115ed52aed9719c6e3
Erica Liu <ericaenjoy3@yahoo.co.uk>
2018-02-14 17:21:07 -0500

    Correcting for a bug

--- callH3K27AC_LoopType.sh
+++ callH3K27AC_LoopType.sh
@@ -8,20 +8,20 @@ for type in ESC; do
 	vchip+=(~/athena/CHIP/CHIP_seq/MergeRep/mergepeak/ReProgram/H3K27AC_${type}/H3K27AC_${type}_common.bed)
 done
 
-sm=ESC
-enh_type=Normal
-cmd="Rscript --no-save --no-restore $SDR/H3K27AC_LoopType.R \
---hichip $DIN/${sm}_H3K27AC.txt \
---vchip ${vchip[@]} \
---bedout $DIN/${sm}_H3K27AC_10KB_${enh_type}_LoopType.txt \
---echip ~/athena/CHIP/PubData/Whyte/doc/Whyte-${enh_type}EnhGencodeEnh.bed"
-echo $cmd
+# sm=ESC
+# enh_type=Super
+# cmd="Rscript --no-save --no-restore $SDR/H3K27AC_LoopType.R \
+# --hichip $DIN/${sm}_H3K27AC.txt \
+# --vchip ${vchip[@]} \
+# --bedout $DIN/${sm}_H3K27AC_10KB_${enh_type}_LoopType.txt \
+# --echip ~/athena/CHIP/PubData/Whyte/doc/Whyte-${enh_type}EnhGencodeEnh.bed"
+# eval $cmd
 
 sm=ESC
 cmd="Rscript --no-save --no-restore $SDR/H3K27AC_LoopType.R \
 --hichip $DIN/${sm}_H3K27AC.txt \
 --vchip ${vchip[@]} \
 --bedout $DIN/${sm}_H3K27AC_10KB_LoopType.txt"
-echo $cmd
+eval $cmd
 
 
