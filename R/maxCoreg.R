adc76a6fe63be6c78b01ccdbc1287aa75cd677e5
Erica Liu <ericaenjoy3@yahoo.co.uk>
2018-03-08 13:25:29 -0500

    Correcting a but with maxCoreg.R

--- R/maxCoreg.R
+++ R/maxCoreg.R
@@ -24,14 +24,14 @@ setMethod(f = "maxCoreg",
     ve <- V(loop.obj@g)$name[V(loop.obj@g)$vtype == type]
     ed <- incident_edges(loop.obj@g, ve)
     
-    # output genes coregulated by the same enhancer hub
+    # output gene coregulated by the same enhancer hub
     dd <- rbindlist(lapply(seq_along(ed), function(i) {
       data.table(hub = names(ed)[i],
         partner = gsub(paste0("\\|{0,1}", names(ed)[i], "\\|{0,1}"), "", as_ids(ed[[i]])),
         connum = length(ed[[i]]), 
         gene = loop_hash[chmatch(as_ids(ed[[i]]), loop), gene1])
     }))
-    idx <- chmatch(dd[, genes], info.obj@gene[, gene])
+    idx <- chmatch(dd[, gene], info.obj@gene[, gene])
     nd <- cbind(dd, info.obj@gene[idx, grep("tpm", colnames(info.obj@gene)), with = FALSE])
     write.table(nd, file = coregfout, row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
 
