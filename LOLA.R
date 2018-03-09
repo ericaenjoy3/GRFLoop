b5660f232d1c1ae9ffbf1a7e0e4af77de8660a50
Erica Liu <ericaenjoy3@yahoo.co.uk>
2018-03-09 13:23:36 -0500

    Add LOLA.R

---
+++ LOLA.R
@@ -0,0 +1,58 @@
+#!/usr/bin/env Rscript
+
+###
+# LOLA on input bed
+# Process:
+###
+
+libfs <- c("LOLA", "data.table", "GenomicRanges", "argparse")
+invisible(sapply(libfs, function(f)suppressPackageStartupMessages(require(f, character.only = T))))
+
+parser <- ArgumentParser()
+parser$add_argument("--bedin", type = "character", required = FALSE,
+  help = "An input bed file.")
+parser$add_argument("--fout", type = "character", required = FALSE,
+  help = "Output LOLA results.")
+# parser$add_argument("--peak", type = "character", nargs='+', required = FALSE,
+#   help = "A peak file of peaks.")
+# parser$add_argument("--config", type = "character", required = FALSE,
+#   help = "Bigwig configuration files.")
+# parser$add_argument("--matf", type = "character", required = FALSE,
+#   help = "Output signal matrix file.")
+args <- parser$parse_args()
+attach(args)
+
+bedin <- file.path("/athena/apostoloulab/scratch/liuyiyua/Andreas_KLF4_HICHIP/doc", "Spec_KLF4_EARLY.bed")
+bedin_dat <- fread(bedin, header = FALSE)
+bedin_dat[, V2 := V2 + 1]
+userSets <- makeGRangesFromDataFrame(bedin_dat,
+	keep.extra.columns = TRUE,
+	seqnames.field = "V1", 
+	start.field = "V2",
+	end.field = "V3",
+)
+
+# bedbg <- file.path("~/athena/PlathChrom", "KPlath_chrom_mm10.bed")
+# bedbg_dat <- fread(bedbg, header = FALSE)
+# userUniverse <- makeGRangesFromDataFrame(bedbg_dat,
+# 	keep.extra.columns = TRUE,
+# 	seqnames.field = "V1", 
+# 	start.field = "V2",
+# 	end.field = "V3",
+# )
+
+chrlenf <- "/home/liuyiyua/athena/Gencode/mm10/sequence/chrNameLengthKnownChr.sizes"
+chrlen_dat <- fread(chrlenf, header = FALSE)
+seqlengths <- chrlen_dat[, V2]
+names(seqlengths) <- chrlen_dat[, V1]
+userUniverse <- tileGenome(seqlengths, tilewidth = 1000, cut.last.tile.in.chrom = TRUE)
+
+dbPath <- file.path("~/athena/LOLA/mm10")
+regionDB <- loadRegionDB(dbPath)
+
+res <- runLOLA(userSets, userUniverse, regionDB, cores=1)
+class(res$userSet) <- "character"
+res[, userSet := gsub(".bed", "", basename(bedin))]
+res <- res[order(pValueLog, decreasing=TRUE),]
+#res <- res[pValueLog!="Inf"]
+fwrite(locResults, file = fout, row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
