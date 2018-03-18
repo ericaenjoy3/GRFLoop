f4759eaaeadba50bfacbe64d21fd3a4c5e448ee4
ericaenjoy3 <ericaenjoy3@yahoo.co.uk>
2018-03-18 18:11:20 -0400

    minor corrections

--- callCoreg.R
+++ callCoreg.R
@@ -6,3 +6,23 @@ for (j in seq_along(libfs)) {
   source(libfs[j])
 }
 
+root <- "/athena/apostoloulab/scratch/liuyiyua/Andreas_H3K27AC_HICHIP/coreg"
+hichipfs <- c("ESC", "MEF", "CONSTANT")
+hichipfs <- file.path("~/athena/Andreas_H3K27AC_HICHIP/doc", paste0("Spec_H3K27AC_", hichipfs, "_LoopType_PromEnh.txt"))
+
+for (hichip in hichipfs) {
+	message(hichip)
+	loop.obj <- loopConst(hichip, score_col = NULL, filterUnknown = TRUE, filterDist = FALSE)
+	info.obj <- infoConst()
+	info.obj <- ProteinCodingInfo(info.obj)
+	info.obj <- TPMInfo(info.obj)
+	obj.list <- infoFilter(loop.obj, info.obj = info.obj)
+	loop.obj <- obj.list[["loop.obj"]]
+	nmax <- maxCoreg(loop.obj, info.obj, coregfout = file.path(root, gsub("(Spec_H3K27AC_[^_]+).+", "\\1_coreg_gene.txt", basename(hichip))))
+	message("maximum contacts in coreg: ", nmax)
+	nmax <- as.numeric(readline("enter nmax: "))
+	shufPlotMulti(loop.obj, info.obj, nmin = 2, nmax = nmax, dout = root, 
+		pdffout = file.path(root, gsub("(Spec_H3K27AC_[^_]+).+", "\\1_Enh_glabBar.pdf", hichip)), 
+		fout = file.path(root, gsub("(Spec_H3K27AC_[^_]+).+", "\\1_coreg_pval.txt", hichip)), 
+		uniqueLoopGene = TRUE)
+}
