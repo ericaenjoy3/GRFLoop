e8d6532355b3dc11cbf299a5421094dd659ac9bb
ericaenjoy3 <ericaenjoy3@yahoo.co.uk>
2018-03-18 17:48:20 -0400

    minor corrections

--- R/maxCoreg.R
+++ R/maxCoreg.R
@@ -23,44 +23,78 @@ setMethod(f = "maxCoreg",
     stopifnot(sum(V(loop.obj@g)$vtype == type) > 0)
     ve <- V(loop.obj@g)$name[V(loop.obj@g)$vtype == type]
     ed <- incident_edges(loop.obj@g, ve)
-    
-    # output gene coregulated by the same enhancer hub
-    # columns:
-    # 1, enh hub coordinates
-    # 2, loop number
-    # 3, promoter fragements,
-    # 3, gene numbers
-    # 4, unique gene numbers
-    # 5, up-regulated unique gene numbers
-    # 6, up-regulated unique gene pair numbers
-    # 7, up-regulated unique genes
-    # filter by unique gene numbers > 1
-    dd <- rbindlist(lapply(seq_along(ed), function(i) {
-      gene <- unlist(lapply(as_ids(ed[[i]]), function(string)loop.obj@loop[loop %in% string, gene1]))
-      uniq_gene <- unique(gene)
-      uniq_gene_stats <- data.table(gene)
-      uniq_gene_stats <- paste(uniq_gene_stats[,.N, by = gene][, paste0(gene, "(", N, ")")], collapse = ",")
-      deg_lab <- sapply(uniq_gene, function(string)info.obj@gene[gene %in% string, DEG_ESC.MEF])
-      idx <- grepl("Up", deg_lab, ignore.case = TRUE)
-      upreg_uniq_gene <- ifelse(sum(idx) > 0, paste(uniq_gene[idx], collapse = ","), "NA")
-      upreg_uniq_gpair_num <- ifelse(sum(idx) >= 2, choose(sum(idx), 2), 0)
-      tmp_dat <- data.table(enh = names(ed)[i],
-        loop_num = length(ed[[i]]),
-        prom = paste(gsub(paste0("\\|{0,1}", names(ed)[i], "\\|{0,1}"), "", as_ids(ed[[i]])), collapse = ","),
-        gene_num = length(gene),
-        uniq_gene_num = length(unique(gene)),
-        upreg_uniq_gene_num = sum(idx),
-        upreg_uniq_gpair_num = upreg_uniq_gpair_num,
-        uniq_gene = uniq_gene_stats,
-        upreg_uniq_gene = upreg_uniq_gene)
-      return(tmp_dat)
-    }))
-    write.table(dd, file = coregfout, row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
+    kpt_num <- sapply(ed, length) >= 2
+
+    # (genunine) extract PromGene from loop slot of loop.obj
+    gene_list <- list()
+    gene_list[[1]] <- lapply(ed[which(kpt_num)], function(es, loop_hash){
+      # potentially multiple loops => mutiple fragments
+      lp <- as_ids(es)
+      dd <- loop_hash[loop %in% lp]
+      stopifnot(all(dd[, all(!is.na(gene1)), by = loop][, V1]))
+      dd <- dd[, unique(gene1), by = loop]
+      gs <- split(dd[, V1], dd[, loop])
+      names(gs) <- NULL
+      return(gs)
+    }, loop_hash = loop.obj@loop)
+    names(gene_list[[1]]) <- NULL
+
+    # (genunine) unique gene sets across connectomes
+    kpt_agset <- !duplicated(gene_list[[1]])
+
+    # (genuine) remove duplicated gene sets within connectomes
+    kpt_wgset <- sapply(gene_list[[1]], function(v_list){
+      nv_list <- unique(v_list);
+      if (length(nv_list) == 1 ) {
+        return(FALSE)
+      } else {
+        return(TRUE)
+      }
+    })
+
+    stopifnot(length(kpt_agset) == length(kpt_wgset))
+    message(sum(!(kpt_agset & kpt_wgset)), " hubs removed due to duplication in idnetical genes contacted within or across hubs.")
+
+    # (genunine) final gene_list
+    gene_list[[1]] <- gene_list[[1]][which(kpt_agset & kpt_wgset)]
+    ve <- ve[which(kpt_num)][which(kpt_agset & kpt_wgset)]
+    ed <- ed[which(kpt_num)][which(kpt_agset & kpt_wgset)]
+
+    # (genuine) gene pairs
+    gpair_list <- list()
+    gpair_list[[1]] <- lapply(gene_list[[1]], function(glist) {
+      gpair <- glist2gpair(glist)
+      return(gpair)
+    })
+
+    deg_list <- list()
+    deg_list[[1]] <- lapply(gpair_list[[1]], function(dd){
+      gene2pairwiseLab(dd, info.obj)
+    })
+
+    dat_list <- lapply(seq(deg_list[[1]]), function(i){
+      idx <- which(deg_list[[1]][[i]] == "2")
+      if (length(idx) == 0) {
+        return(NA)
+      }
+      dd  <- gpair_list[[1]][[i]][idx]
+      genes <- c(dd[, Var1], dd[, Var2]) %>% unique()
+      data.table(
+        enh = ve[i],
+        connum = length(ed[[i]]),
+        prom = paste(gsub(paste0("\\|{0,1}", ve[i], "\\|{0,1}"), "", as_ids(ed[[i]])), collapse = ","),
+        genes = paste(genes, collapse = ",")
+      )  
+    })
+    kpt_coreg <- !is.na(dat_list)
+    dat <- rbindlist(dat_list[kpt_coreg])
+    write.table(dat, file = coregfout, row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
 
     # max connection for which the number of hub > 1
     maxco <- data.table(connum = sapply(ed, length))
     maxco <- maxco[, .N, by = connum]
 
     return(maxco[N>1, max(connum)])
+    
   }
 )
