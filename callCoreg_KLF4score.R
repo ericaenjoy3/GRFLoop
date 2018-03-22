261f22b82c0f1862cfb9319ac28723281c611beb
ericaenjoy3 <ericaenjoy3@yahoo.co.uk>
2018-03-22 00:38:58 -0400

    Correcting for bugs

--- callCoreg_KLF4score.R
+++ callCoreg_KLF4score.R
@@ -7,7 +7,8 @@ for (j in seq_along(libfs)) {
 }
 
 root <- "/athena/apostoloulab/scratch/liuyiyua/Andreas_KLF4_HICHIP"
-dout <- file(root, "coreg_KLF4score")
+dout <- file.path(root, "coreg_KLF4score")
+dir.create(dout, showWarnings = FALSE, recursive = TRUE)
 
 hichip <- file.path("~/athena/Andreas_H3K27AC_HICHIP/doc", "Spec_H3K27AC_ESC_LoopType_PromEnh.txt")
 
@@ -24,9 +25,49 @@ loop.obj <- obj.list[["loop.obj"]]
 obj.list <- selCoreg(loop.obj, info.obj = info.obj)
 loop.obj <- obj.list[["loop.obj"]]
 
-fs <- file.path(root, paste0(c("DAY3", "DAY6", "ESC"), "_KLF4_50K_H3K27AC_LoopType.txt"))
-
-loop.obj.list <- list()
-for (f in fs) {
-	loop.obj.list[[length(loop.obj.list) + 1]] <- loopConst(f, score_col = 11, filterUnknown = TRUE, filterDist = FALSE)
+sortlabs <- function(string) {
+	vec <- c(gsub("(.+)\\|(.+)", "\\1", string), gsub("(.+)\\|(.+)", "\\2", string))
+	paste(mixedsort(vec), collapse = "|")
 }
+
+dat_list[[1]] <- data.table(loop = sapply(as_ids(E(loop.obj@g)), sortlabs), key = "loop")
+
+fs <- file.path(root, "doc", paste0(c("DAY3", "DAY6", "ESC"), "_KLF4_50K_H3K27AC_LoopType.txt"))
+
+counter <- 1
+for (f in fs) {
+	counter <- counter + 1
+	tmp.loop.obj <- loopConst(f, score_col = 11, filterUnknown = FALSE, filterDist = FALSE)
+	ng <- intersection(tmp.loop.obj@g, loop.obj@g, keep.all.vertices = FALSE)
+	dat_list[[counter]] <- data.table(loop = sapply(as_ids(E(ng)), sortlabs), score = E(ng)$score, key = "loop")
+}
+dat <- Reduce(function(...) merge(..., all = TRUE), dat_list)
+dat[is.na(dat)] <- 0
+setnames(dat, colnames(dat)[-1], c("DAY3", "DAY6", "ESC"))
+ndat <- copy(dat)
+ndat[ndat>0] <- 1
+
+order2 <- function(..., decreasing = TRUE){ order(..., decreasing = decreasing) }
+row_idx <- do.call(order2, ndat[,ncol(ndat):2, with = FALSE])
+
+# bks <- c(0, seq(0, 10, 1), seq(20, dat[, max(.SD), .SDcols = colnames(dat)[-1]], 10))
+bks <- c(0, 1)
+colors <- colorRampPalette(RColorBrewer::brewer.pal(n = 9, name = "Reds"))(length(bks))
+
+ht_list <- ComplexHeatmap::Heatmap(dat[row_idx, !"loop", with = FALSE],
+        col = circlize::colorRamp2(bks, colors),
+        show_row_names = FALSE,
+        show_column_names = TRUE,
+        cluster_rows = FALSE,
+        cluster_columns = FALSE,
+        use_raster = FALSE,
+        show_row_dend = FALSE,
+        show_column_dend = FALSE,
+        combined_name_fun = NULL,
+        name = "score",
+        show_heatmap_legend = FALSE)
+
+pdffout <- file.path(dout, "H3K27AC_Coreg_Loop_KLF4_Loop_Score.pdf")
+pdf(pdffout)
+draw(ht_list)
+dev.off()
